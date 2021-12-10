f4eaba9349c61dde08bc4f095e254129c72de57e
Richard Hess <eswat2@gmail.com>
2021-12-09 16:26:35 -0800

    fix tests and plugin

--- index.js
+++ index.js
@@ -26,47 +26,51 @@ const getSizes = (stop = 900, start = 0) => {
   return sizeObj
 }
 
+const generate = (options) => {
+  const tags = [
+    'fontSize',
+    'height',
+    'lineHeight',
+    'maxHeight',
+    'maxWidth',
+    'minHeight',
+    'minWidth',
+    'spacing',
+    'width',
+  ]
+
+  const output = {
+    theme: {
+      extend: {},
+    },
+  }
+
+  if (options) {
+    const keys = Object.keys(options)
+
+    keys.forEach((key) => {
+      if (tags.includes(key)) {
+        // NOTE:  support the new and old option props...
+        const stop = options[key].stop || options[key].total || 900
+        const start = options[key].start || options[key].startingValue || 0
+
+        const results = getSizes(stop, start)
+
+        output.theme.extend[key] = { ...results }
+      }
+    })
+  }
+
+  return output
+}
+
 module.exports = plugin.withOptions(
-  function (options) {
-    return function ({ addUtilities, e, variants, theme }) {
+  function (_options) {
+    return function () {
       // ...
     }
   },
   function (options) {
-    const tags = [
-      'fontSize',
-      'height',
-      'lineHeight',
-      'maxHeight',
-      'maxWidth',
-      'minHeight',
-      'minWidth',
-      'spacing',
-      'width',
-    ]
-
-    const output = {
-      theme: {
-        extend: {},
-      },
-    }
-
-    if (options) {
-      const keys = Object.keys(options)
-
-      keys.forEach((key) => {
-        if (tags.includes(key)) {
-          // NOTE:  support the new and old option props...
-          const stop = options[key].stop || options[key].total || 900
-          const start = options[key].start || options[key].startingValue || 0
-
-          const results = getSizes(stop, start)
-
-          output.theme.extend[key] = { ...results }
-        }
-      })
-    }
-
-    return output
+    return generate(options)
   }
 )
